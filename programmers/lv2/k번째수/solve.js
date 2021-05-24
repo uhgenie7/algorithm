104fca3f0f4b668df83b1fe5bed288810b1bdacc
uhj1993 <dzzd777@naver.com>
2021-05-24 12:52:07 +0900

    solve: k번째수

---
+++ programmers/lv2/k번째수/solve.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+  var answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    let result = array
+      .slice(commands[i][0] - 1, commands[i][1])
+      .sort((a, b) => a - b);
+    answer.push(result[commands[i][2] - 1]);
+  }
+  return answer;
+}
