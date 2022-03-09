3c89859f3f1d4272975c1a513aa6b4d055f555b6
HyejinUm <72803184+uhj1993@users.noreply.github.com>
2022-03-09 21:51:38 +0900

    [Silver V] Title: 셀프 넘버, Time: 172 ms, Memory: 11208 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -0,0 +1,30 @@
+function NotSelfNumber(N) {
+  let sum = N;
+
+  while (true) {
+    if (N === 0) break;
+    sum += N % 10;
+    N = parseInt(N / 10);
+  }
+
+  return sum;
+}
+
+function selfnumber(N) {
+  let selfnum = [];
+  let result = [];
+
+  for (let i = 1; i <= N; i++) {
+    let index = NotSelfNumber(i);
+    if (index <= N) {
+      selfnum[index] = true;
+    }
+  }
+
+  for (let i = 1; i <= N; i++) {
+    if (!selfnum[i]) result.push(i);
+  }
+  console.log(result.join("\n"));
+}
+
+selfnumber(10000);
