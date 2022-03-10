3e41c07b7c43862615bd478b8e149aeb7eb614bc
HyejinUm <72803184+uhj1993@users.noreply.github.com>
2022-03-10 18:20:25 +0900

    Update 다이얼.js

--- 백준/Bronze/5622. 다이얼/다이얼.js
+++ 백준/Bronze/5622. 다이얼/다이얼.js
@@ -52,3 +52,32 @@ for (let i = 0; i < input.length; i++) {
 }
 
 console.log(time);
+
+
+    // ============================ 풀이 2 ===============================
+
+    // 모든 숫자는 걸기 위해서 1초씩 더 걸린다.
+    let minTime = alphabets.length;
+
+    for (let i = 0; i < alphabets.length; i++) {
+        const ascii = alphabets[i].charCodeAt();
+
+        if (ascii <= 'C'.charCodeAt()) {
+            minTime += 2;
+        } else if (ascii <= 'F'.charCodeAt()) {
+            minTime += 3;
+        } else if (ascii <= 'I'.charCodeAt()) {
+            minTime += 4;
+        } else if (ascii <= 'L'.charCodeAt()) {
+            minTime += 5;
+        } else if (ascii <= 'O'.charCodeAt()) {
+            minTime += 6;
+        } else if (ascii <= 'S'.charCodeAt()) {
+            minTime += 7;
+        } else if (ascii <= 'V'.charCodeAt()) {
+            minTime += 8;
+        } else if (ascii <= 'Z'.charCodeAt()) {
+            minTime += 9;
+        }
+    }
+
