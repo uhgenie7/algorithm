0a8b8304ec16180062b151b49f90590ebafa04be
HyejinUm <uhj1993@naver.com>
2022-12-30 22:22:55 +0900

    A를 #으로 (2)

--- inflearn/section1/A를 #으로/solve.js
+++ inflearn/section1/A를 #으로/solve.js
@@ -1,8 +1,18 @@
+let str = 'BANANA';
+
 function solution(s) {
   let answer = '';
   answer = s.replaceAll('A', '#');
   return answer;
 }
 
-let str = 'BANANA';
+function solution2(s) {
+  let answer = '';
+  for (let x of s) {
+    x === 'A' ? (answer += '#') : (answer += x);
+  }
+  return answer;
+}
+
 console.log(solution(str));
+console.log(solution2(str));
