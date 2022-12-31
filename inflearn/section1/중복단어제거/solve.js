13512652d79132f5e6d01d43a7bd5d997dfc05da
HyejinUm <uhj1993@naver.com>
2022-12-31 21:35:50 +0900

    🔥 중복 단어 제거

--- inflearn/section1/중복단어제거/solve.js
+++ inflearn/section1/중복단어제거/solve.js
@@ -7,5 +7,13 @@ function solution(s) {
   }
   return answer;
 }
+
+function solution2(s) {
+  let answer;
+  answer = s.filter((x, i) => s.indexOf(x) === i);
+  return answer;
+}
+
 let str = ['good', 'time', 'good', 'time', 'student'];
 console.log(solution(str));
+console.log(solution2(str));
