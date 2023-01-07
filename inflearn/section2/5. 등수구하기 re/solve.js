8b013bd34bd6b839698e2a0bd1dea3e020a7c543
HyejinUm <uhj1993@naver.com>
2023-01-07 17:51:00 +0900

    🔥 등수구하기

--- inflearn/section2/5. 등수구하기 re/solve.js
+++ inflearn/section2/5. 등수구하기 re/solve.js
@@ -1,6 +1,14 @@
 function solution(arr) {
-  let answer = [];
+  let n = arr.length;
+  let answer = Array.from({ length: n }, () => 1);
 
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[j] > arr[i]) {
+        answer[i]++;
+      }
+    }
+  }
   return answer;
 }
 
