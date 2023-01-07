5a0409d570c71698f5e4ce00ff5e52fb50fcc53d
HyejinUm <uhj1993@naver.com>
2023-01-07 18:42:30 +0900

    🔥 봉우리

--- inflearn/section2/7. 봉우리 re/solve.js
+++ inflearn/section2/7. 봉우리 re/solve.js
@@ -1,5 +1,29 @@
 function solution(arr) {
   let answer = 0;
+  let length = arr.length;
+  // 12, 3, 6, 9
+  let aroundArray1 = [-1, 0, 1, 0];
+  let aroundArray2 = [0, 1, 0, -1];
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      let flag = 1;
+      for (let a = 0; a < 4; a++) {
+        let aroundx = i + aroundArray1[a];
+        let aroundy = j + aroundArray2[a];
+        if (
+          aroundx >= 0 &&
+          aroundx < length &&
+          aroundy >= 0 &&
+          aroundy < length &&
+          arr[aroundx][aroundy] >= arr[i][j]
+        ) {
+          flag = 0;
+          break;
+        }
+      }
+      if (flag) answer++;
+    }
+  }
   return answer;
 }
 
