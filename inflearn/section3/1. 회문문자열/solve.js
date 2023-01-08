dd10af25c50bcd17a2a2813327a406fdafc4f366
HyejinUm <uhj1993@naver.com>
2023-01-08 13:20:16 +0900

    회문문자열

--- inflearn/section3/1. 회문문자열/solve.js
+++ inflearn/section3/1. 회문문자열/solve.js
@@ -1,6 +1,15 @@
 function solution(s) {
+  let answer = 'YES';
+  const word = s.toLowerCase();
+  let a = parseInt(word.length / 2);
+  const wordArray = [...word];
+  for (let i = 0; i < a; i++) {
+    if (wordArray[i] !== wordArray[wordArray.length - i - 1]) {
+      answer = 'NO';
+    }
+  }
   return answer;
 }
 
-let str = 'goooG';
+let str = 'googoaG';
 console.log(solution(str));
