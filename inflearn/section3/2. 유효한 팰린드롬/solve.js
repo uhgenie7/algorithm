7ddcb81dd58d71f0dce40a84607f25197cfe1b91
HyejinUm <uhj1993@naver.com>
2023-01-08 14:21:17 +0900

    숫자만 추출

--- inflearn/section3/2. 유효한 팰린드롬/solve.js
+++ inflearn/section3/2. 유효한 팰린드롬/solve.js
@@ -1,6 +1,7 @@
 function solution(s) {
+  let answer = 'YES';
+
   return answer;
 }
-
 let str = 'found7, time: study; Yduts; emit, 7Dnuof';
 console.log(solution(str));
--- inflearn/section3/3. 숫자만 추출/solve.js
+++ inflearn/section3/3. 숫자만 추출/solve.js
@@ -1,4 +1,8 @@
-function solution(str) {}
+function solution(str) {
+  let answer = str.replace(/[^0-9]/g, '');
+  answer = parseInt(answer);
+  return answer;
+}
 
 let str = 'g0en2T0s8eSoft';
 console.log(solution(str));
