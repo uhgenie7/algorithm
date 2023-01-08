260a4e1955c11269c2ba5e5dd7cc60f0243c56bb
HyejinUm <uhj1993@naver.com>
2023-01-08 15:33:08 +0900

    유효한 팰린드롬

--- inflearn/section3/2. 유효한 팰린드롬/solve.js
+++ inflearn/section3/2. 유효한 팰린드롬/solve.js
@@ -1,6 +1,7 @@
 function solution(s) {
   let answer = 'YES';
-
+  let word = s.toLowerCase().replace(/[^a-z]/g, '');
+  if (word.split('').reverse().join('') !== word) return 'NO';
   return answer;
 }
 let str = 'found7, time: study; Yduts; emit, 7Dnuof';
