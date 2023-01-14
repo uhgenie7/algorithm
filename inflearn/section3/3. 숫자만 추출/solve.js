2fff1f860963f855540c6c7d5a3430c73beff49d
HyejinUm <uhj1993@naver.com>
2023-01-14 14:34:54 +0900

    숫자만 추출 (parseInt 없는 버전)

--- inflearn/section3/3. 숫자만 추출/solve.js
+++ inflearn/section3/3. 숫자만 추출/solve.js
@@ -12,6 +12,15 @@ function solution2(str) {
   return parseInt(answer);
 }
 
+function solution3(str) {
+  let answer = 0;
+  for (let x of str) {
+    if (!isNaN(x)) answer = answer * 10 + Number(x);
+  }
+  return answer;
+}
+
 let str = 'g0en2T0s8eSoft';
 console.log(solution(str));
 console.log(solution2(str));
+console.log(solution3(str));
